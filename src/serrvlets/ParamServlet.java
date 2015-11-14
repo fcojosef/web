37f4056ea3a464ef964bfdfb33161be1bf6c58e3
fcojosef.o@gmail.com <fcojosef.o@gmail.com>
2015-11-14 12:50:38 -0300

    Alterado index para novo servlet

--- src/serrvlets/ParamServlet.java
+++ src/serrvlets/ParamServlet.java
@@ -16,14 +16,24 @@ public class ParamServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		response.setContentType("text/html");
+		try{
+		
+		String nome = request.getParameter("nome");
+		String email = request.getParameter("email");
+		int idade = Integer.parseInt(request.getParameter("idade"));
+		
 		PrintWriter out = response.getWriter();
 		out.println("<html><body><ul>");
 		out.println("<li>Param 1:" + request.getParameter("nome"));
 		out.println("<li>Param 2:" + request.getParameter("senha"));
 		out.println("</ul></body></html>");
 		out.close();
-		
-	}
+		response.sendRedirect("sucesso.html");
+		}catch (Exception ex){
+				response.sendRedirect("cadastrofalhou.html");
+		 	}
+		}
+	
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
